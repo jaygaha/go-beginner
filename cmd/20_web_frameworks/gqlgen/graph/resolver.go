0d74892a5169feae78e686283a6f715dcc92d04e
Jay Gaha <gaha@buzzreach.co.jp>
2025-05-12 19:43:03 +0900

    day #39 gqlgen ↪️↩️

---
+++ cmd/20_web_frameworks/gqlgen/graph/resolver.go
@@ -0,0 +1,12 @@
+package graph
+
+import "github.com/jaygaha/go-beginner/cmd/20_web_frameworks/gqlgen/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+// This file will not be regenerated automatically.
+type Resolver struct {
+	books []*model.Book
+}
